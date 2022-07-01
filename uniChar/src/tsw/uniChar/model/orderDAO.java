419fd38c2bb3a2e96ff7aa669b77c1ad1f6096e3
mercury501 <tommaso.desoldaee@gmail.com>
2022-07-01 16:15:14 +0200

    creata dao ordini

--- uniChar/src/tsw/uniChar/model/orderDAO.java
+++ uniChar/src/tsw/uniChar/model/orderDAO.java
@@ -4,8 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
 
 import tsw.uniChar.Beans.productBean;
+import tsw.uniChar.Beans.cartBean;
 import tsw.uniChar.Beans.orderBean;;
 
 public class orderDAO {
@@ -20,18 +22,65 @@ public class orderDAO {
 		}
 	}
 	
-	public void insertOrder(orderBean[] order) throws SQLException{
-		String sql = "INSERT INTO ORDERS VALUES(?, ?, ?, ?, ?, ?, ?)";
-		
+	public void insertOrder(orderBean order) throws SQLException {
+		int orderID = getNextOrderID();
+
+		String sql = "INSERT INTO ORDERS VALUES(?, ?, ?, CURDATE(), ?, ?, ?)";
+
 		try {
 			statement = sqlConn.prepareStatement(sql);
-		for (productBean prod : products) {
-			statement.setInt(1, );
+			statement.setInt(1, order.getUserID());
+			statement.setInt(2, orderID);
+			statement.setInt(5, 2);
+
+			for (Map.Entry<Integer, productBean> set : order.getCart().getProducts().entrySet()) {
+
+				statement.setInt(3, set.getKey());
+				statement.setInt(4, order.getCart().getQuantity(set.getKey()));
+				statement.setFloat(6, set.getValue().getPrezzo());
+
+				statement.executeUpdate();
+
+			}
+			statement.close();
+			sqlConn.commit();
+			releaseConn();
+
+		} catch (SQLException e) {
+			e.printStackTrace(System.out);
 		}
+
+	}
+	
+	private int getNextOrderID() throws SQLException {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		int nextOrder = -1;
+		
+		String sql = "SELECT MAX(ORDER_ID) + 1 FROM ORDERS ";
+		try {
+			ps = sqlConn.prepareStatement(sql);
+			
+			rs = ps.executeQuery();
+			
+			if (rs.next())
+				nextOrder = rs.getInt(1);
+			
+			
+				
+			
 		}catch (SQLException e) {
 			
 		}
 		
+		return nextOrder;
+		
 	}
-	
+	private void releaseConn() throws SQLException{
+		try {
+			DriverManagerConnectionPool.releaseConnection(sqlConn);
+		}catch (SQLException e) {
+			throw e;
+		}
+	}
 }
