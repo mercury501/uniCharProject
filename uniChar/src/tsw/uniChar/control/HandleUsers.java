4ab281801bac7dbe9f90d8e976b38b72610686c5
Luca Pastore <103969472+PastoreLuca@users.noreply.github.com>
2022-07-12 00:24:43 +0200

    piccoli cambiamenti

--- uniChar/src/tsw/uniChar/control/HandleUsers.java
+++ uniChar/src/tsw/uniChar/control/HandleUsers.java
@@ -50,103 +50,103 @@ public class HandleUsers extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
         {
-            
-         
+
+
             RequestDispatcher dispatcher = null;
 			String returnTo = (String)request.getParameter("returnto");
-            
+
             try {
             	String action = (String)request.getParameter("action");
-        		
+
         		if (action == null)
         			action = (String)request.getAttribute("action");
-            	
+
 			if (action.equals("login")) {
 				response.sendRedirect(loginHandler(request, response));
-				
+
 				return;
 			}
 			if (action.equals("register")) {
 				dispatcher = getServletContext().getRequestDispatcher(registerHandler(request, response));
 				dispatcher.forward(request, response);
-				
+
 				return;
 			}
 			if (action.equals("changePass")) {
 				dispatcher = getServletContext().getRequestDispatcher(changePassHandler(request, response));
 				dispatcher.forward(request, response);
-				
+
 				return;
 			}
-			
+
 			if(action.equalsIgnoreCase("logout")) {
 				LogOut(request,response);
-				
-			
+
+
 			}
-			
-			
+
+
 			if(action.equalsIgnoreCase("users")) {
-				
+
 				userBean uB = new userBean();
-				
-				
+
+
 				System.out.print(uB.getRole());
-				
+
 					userDAO uD = new userDAO();
-					
+
 					List<userBean> listaUtenti = new ArrayList<userBean>();
-					
+
 					listaUtenti = uD.showUsers(uB);
-					
-		
-					
-					
+
+
+
+
 					request.removeAttribute("users");
 					request.setAttribute("users", listaUtenti);
-					
-					
-					
-			
+
+
+
+
 			}
-			
+
 			if(action.equalsIgnoreCase("user")) {
-				
+
 				userBean uB = new userBean();
 				userDAO uD = new userDAO();
-				
-				
+
+
 				HttpSession session = request.getSession(false);
 				String email = (String) session.getAttribute("email");
-				
+
 				uB = uD.getUser(email);
-				
-				
+
+
 				request.removeAttribute("user");
 				request.setAttribute("user", uB);
-				
-				
+
+
 			}
-			
-		       
+
+
             if(!action.equalsIgnoreCase("logout")) {
-    
+
         	dispatcher = getServletContext().getRequestDispatcher("/" + returnTo);
     			dispatcher.forward(request, response);
-			
+
             }
-				
+
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-     
- 
+
+
           }
-    
+
 	private String loginHandler(HttpServletRequest request, HttpServletResponse response) throws SQLException {
-		String forward = "index.jsp";
-		String loginError = "invalidLogin.html";
+		String forward = "AreaUtente.jsp";
+		String loginError = "invalidLogin.jsp";
 		String adminPage = "AreaAdmin.jsp";
 
 		userDAO uDs = new userDAO();
@@ -160,7 +160,7 @@ public class HandleUsers extends HttpServlet {
 				// recupero la sessione
 				HttpSession oldSession = request.getSession(false);
 				cartBean cart = (cartBean) request.getSession().getAttribute("cart");
-				
+
 				if (oldSession != null) {
 					oldSession.invalidate(); // invalido la sessione
 				}
@@ -173,8 +173,8 @@ public class HandleUsers extends HttpServlet {
 				currentSession.setAttribute("name", uB.getName());
 				currentSession.setAttribute("email", uB.getEmail());
 				currentSession.setAttribute("role", uB.getRole());
-				currentSession.setMaxInactiveInterval(5 * 60); // 5 min di inattivita  massima
-				
+				currentSession.setMaxInactiveInterval(5 * 60); // 5 min di inattivitaï¿½ massima
+
 				System.out.println(uB.getRole());
 				System.out.println(uB.getEmail());
 				System.out.println(uB.getId());
@@ -192,7 +192,7 @@ public class HandleUsers extends HttpServlet {
 			throw e;
 		}
 	}
-	
+
 	private String registerHandler(HttpServletRequest request, HttpServletResponse response) throws SQLException {
 		String forward = "/index.jsp";
 
@@ -202,7 +202,7 @@ public class HandleUsers extends HttpServlet {
 		String user = request.getParameter("username");
 		String password = request.getParameter("password");
 		String email = request.getParameter("email");
-		
+
 		userBean uB = new userBean();
 		uB.setName(name);
 		uB.setSurname(surname);
@@ -223,43 +223,43 @@ public class HandleUsers extends HttpServlet {
 			return forward;
 		}
 	}
-	
+
 	public void LogOut(HttpServletRequest request, HttpServletResponse response){
-		
-		
+
+
 		String forward = "index.jsp";
-		
+
 		// recupero la sessione
 		HttpSession oldSession = request.getSession(false);
 
 		if (oldSession != null) {
 			oldSession.invalidate(); // invalido la sessione
 		}
-		
+
 		try {
 			response.sendRedirect(forward);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
+
 	}
-	
-	
+
+
 	private String changePassHandler(HttpServletRequest request, HttpServletResponse response) throws SQLException {
 		String forward = "/changePassword.jsp";
-		
+
 		userDAO uDs = new userDAO();
-		
+
 		HttpSession currentSession = request.getSession(); // creo una nuova connessione
 		String user = (String) currentSession.getAttribute("user");
-		
-		
+
+
 
 		String oldPassword = request.getParameter("oldpass");
 		String newPassword = request.getParameter("newpass");
-		
+
 		userBean uB = new userBean();
 		uB.setUser(user);
 		uB.setPassword(oldPassword);
