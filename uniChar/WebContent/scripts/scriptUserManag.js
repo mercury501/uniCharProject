58644a8377b2aa2172cdd123baaab981631ec4da
mercury501 <tommaso.desoldaee@gmail.com>
2022-07-14 10:55:23 +0200

    controllo email in gestione utenti

--- uniChar/WebContent/scripts/scriptUserManag.js
+++ uniChar/WebContent/scripts/scriptUserManag.js
@@ -1,6 +1,18 @@
 function validate(obj) {	
 			var valid = true;
-
+			
+			var email = document.getElementsByName("email")[0];
+			console.log('Mail:');
+			console.log(email);
+			if(!checkEmail(email)) {
+				valid = false;
+				document.getElementById("errorMail").style.display = 'block';
+				document.getElementById("errorMessageMail").style.display = 'block';
+			} else {
+				document.getElementById("errorMail").style.display = 'none';
+				document.getElementById("errorMessageMail").style.display = 'none';
+				document.getElementById("errorMessageMailUsed").style.display = 'none';
+				
 				var emailTestPass = document.getElementsByName("email")[0].value;
 				console.log(emailTestPass);
 		        $.ajax({
@@ -24,6 +36,15 @@ function validate(obj) {
 		            	console.log(jqXHR);
 		            } 
 		        }); 
-				
+			}
 			if(valid) obj.submit();
 		}
+
+
+function checkEmail(inputtxt) {
+	var email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if(inputtxt.value.match(email)) 
+		return true;
+	
+	return false;	
+}
